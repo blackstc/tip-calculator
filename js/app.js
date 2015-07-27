b2afb34adc7ad0a3f5254db1cf89aa0ad2d30e5b
Blackstock <charlieblackstock@gmail.com>
2015-07-26 21:40:50 -0600

    added JS and DOM elements to make button calculate 20% tip

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-var calcBtn = getElementById('calc');
+var calcBtn = document.getElementById('calc');
+var result = document.getElementById('result');
 
 calcBtn.addEventListener('click', function() {
-
-})
+  var calcAmount = document.getElementById('tip').value;
+  result.innerHTML = "You should tip $" + Number(calcAmount * 0.2);
+});
