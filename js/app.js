a98f8f52408c65c4696de893feae45201c24a0b4
Blackstock <charlieblackstock@gmail.com>
2015-07-27 09:11:43 -0600

    completed tip calculator with drop-down tip selectors

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 var calcBtn = document.getElementById('calc');
 var result = document.getElementById('result');
+var optionList = document.getElementById('tipPercentage');
+var tipAmount = optionList.selectedOptions[0].value;
 
 calcBtn.addEventListener('click', function() {
+  result.innerHTML = "";
+  var tipAmount = optionList.selectedOptions[0].value;
   var calcAmount = document.getElementById('tip').value;
-  result.innerHTML = "You should tip $" + Number(calcAmount * 0.2);
+  result.innerHTML = "You should tip $" + Number(calcAmount * tipAmount).toFixed(2);
+  //toFixed adds additional floats to response
 });
